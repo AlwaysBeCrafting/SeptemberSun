b680eda307d9aa2c5bec66acd18d293f64e19e5d
Mark Halls <fireraisr@gmail.com>
2016-09-11 16:32:22 -0700

    fixed spacing

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -5,14 +5,12 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.MyGdxGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-        config.width = 320;
-        config.height = 180;
-        
-
+		config.width = 320;
+		config.height = 180;
 
 
 		new LwjglApplication(new MyGdxGame(), config);
-                }
-                }
+	}
+}
