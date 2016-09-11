0c945a7536375369e076821aeb877fb6ebec573b
Mark Halls <fireraisr@gmail.com>
2016-09-11 16:04:43 -0700

    updated window size

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import com.mygdx.game.MyGdxGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 320;
+        config.height = 180;
+        
+
+
+
 		new LwjglApplication(new MyGdxGame(), config);
-	}
-}
+                }
+                }
