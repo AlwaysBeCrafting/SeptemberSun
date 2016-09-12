b2bf4e77ef0cb6bbc019efa8456a97ddaf52ace2
Mark Halls <fireraisr@gmail.com>
2016-09-11 17:01:00 -0700

    added fella.png sprite in place of badlogic.jpg

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -13,7 +13,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("fella.png");
 	}
 
 	@Override
