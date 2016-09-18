4cc9d56256abd31016f02d6d15e74363c6dd7812
Alexander Prime <rainesabaal@gmail.com>
2016-09-18 16:11:24 -0700

    Use new Colors and Solarized classes for clear color

--- core/src/stream/alwaysbecrafting/septembersun/SeptemberSun.java
+++ core/src/stream/alwaysbecrafting/septembersun/SeptemberSun.java
@@ -6,38 +6,47 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import stream.alwaysbecrafting.septembersun.util.Colors;
+
+import static stream.alwaysbecrafting.septembersun.util.Colors.Solarized.BASE03;
+
+
+//==============================================================================
 public class SeptemberSun extends ApplicationAdapter {
+	//--------------------------------------------------------------------------
+
 	SpriteBatch batch;
 	Texture img;
-	
-	@Override
-	public void create () {
+
+	//--------------------------------------------------------------------------
+
+	@Override public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("fella.png");
+		img = new Texture( "fella.png" );
 	}
 
-	@Override
-	public void render () {
-		int base03 = Solarized.BASE03;
-		int r = (base03 & 0x00ff0000) >> 16;
-		int g = (base03 & 0x0000ff00) >> 8;
-		int b = (base03 & 0x000000ff);
+	//--------------------------------------------------------------------------
 
+	@Override public void render () {
 		Gdx.gl.glClearColor(
-				(1 / 255f) * r,
-				(1 / 255f) * g,
-				(1 / 255f) * b,
-				1
-		);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+				Colors.r( BASE03 ),
+				Colors.g( BASE03 ),
+				Colors.b( BASE03 ),
+				Colors.a( BASE03 ));
+
+		Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT );
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw( img, 0, 0 );
 		batch.end();
 	}
+
+	//--------------------------------------------------------------------------
 	
-	@Override
-	public void dispose () {
+	@Override public void dispose () {
 		batch.dispose();
 		img.dispose();
 	}
+
+	//--------------------------------------------------------------------------
 }
+//------------------------------------------------------------------------------
