0d99077a19004d2339e3e71d41d3741529b952f7
Alexander Prime <rainesabaal@gmail.com>
2016-09-18 16:25:28 -0700

    Add VelocityCompoment

---
+++ core/src/stream/alwaysbecrafting/septembersun/component/VelocityComponent.java
@@ -0,0 +1,14 @@
+package stream.alwaysbecrafting.septembersun.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.math.Vector2;
+
+//==============================================================================
+public class VelocityComponent implements Component {
+	//--------------------------------------------------------------------------
+
+	Vector2 velocity = new Vector2();
+
+	//--------------------------------------------------------------------------
+}
+//------------------------------------------------------------------------------
