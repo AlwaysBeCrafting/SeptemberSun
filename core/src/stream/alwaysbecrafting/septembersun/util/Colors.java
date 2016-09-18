c6a6dabb0781c584572c416a09b1cbde2d434c27
Alexander Prime <rainesabaal@gmail.com>
2016-09-18 15:56:40 -0700

    Move Solarized to static class inside Colors

--- core/src/stream/alwaysbecrafting/septembersun/util/Colors.java
+++ core/src/stream/alwaysbecrafting/septembersun/util/Colors.java
@@ -35,5 +35,30 @@ public abstract class Colors {
 	}
 
 	//--------------------------------------------------------------------------
+
+	//==========================================================================
+	public static abstract class Solarized {
+		//----------------------------------------------------------------------
+
+		public static final int BASE03  = 0xff002b36;
+		public static final int BASE02  = 0xff073642;
+		public static final int BASE01  = 0xff586e75;
+		public static final int BASE00  = 0xff657b83;
+		public static final int BASE0   = 0xff839496;
+		public static final int BASE1   = 0xff93a1a1;
+		public static final int BASE2   = 0xffeee8d5;
+		public static final int BASE3   = 0xfffdf6e3;
+		public static final int YELLOW  = 0xffb58900;
+		public static final int ORANGE  = 0xffcb4b16;
+		public static final int RED     = 0xffdc322f;
+		public static final int MAGENTA = 0xffd33682;
+		public static final int VIOLET  = 0xff6c71c4;
+		public static final int BLUE    = 0xff268bd2;
+		public static final int CYAN    = 0xff2aa198;
+		public static final int GREEN   = 0xff859900;
+
+		//----------------------------------------------------------------------
+	}
+	//--------------------------------------------------------------------------
 }
 //------------------------------------------------------------------------------
