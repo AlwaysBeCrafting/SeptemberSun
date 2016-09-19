9f283d0786613fc0c4a0c70bfdd09669eb55d58c
Alexander Prime <rainesabaal@gmail.com>
2016-09-18 17:24:10 -0700

    Instantiate a Vector2 in PositionComponent

--- core/src/stream/alwaysbecrafting/septembersun/component/PositionComponent.java
+++ core/src/stream/alwaysbecrafting/septembersun/component/PositionComponent.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.math.Vector2;
 public class PositionComponent implements Component {
 	//--------------------------------------------------------------------------
 
-	public Vector2 position;
+	public Vector2 position = new Vector2();
 
 	//--------------------------------------------------------------------------
 }
