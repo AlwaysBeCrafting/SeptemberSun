cee9b44433c7641ce2894b58b605140f399ad593
Alexander Prime <rainesabaal@gmail.com>
2016-09-18 17:34:48 -0700

    Add SpriteBatch begin and end to render loop

--- core/src/stream/alwaysbecrafting/septembersun/system/SpriteRenderSystem.java
+++ core/src/stream/alwaysbecrafting/septembersun/system/SpriteRenderSystem.java
@@ -1,14 +1,20 @@
 package stream.alwaysbecrafting.septembersun.system;
 
+import com.badlogic.ashley.core.Engine;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import stream.alwaysbecrafting.septembersun.component.PositionComponent;
 import stream.alwaysbecrafting.septembersun.component.SpriteComponent;
 
 //==============================================================================
 public class SpriteRenderSystem extends IteratingSystem {
+	//--------------------------------------------------------------------------
+
+	SpriteBatch batcher;
+
 	//--------------------------------------------------------------------------
 
 	public static SpriteRenderSystem create() {
@@ -28,7 +34,25 @@ public class SpriteRenderSystem extends IteratingSystem {
 
 	//--------------------------------------------------------------------------
 
+	@Override public void addedToEngine( Engine e ) {
+		super.addedToEngine( e );
+		batcher = new SpriteBatch();
+	}
+
+	//--------------------------------------------------------------------------
+
+	@Override public void update( float deltaTime ) {
+		batcher.begin();
+
+		super.update( deltaTime );
+
+		batcher.end();
+	}
+
+	//--------------------------------------------------------------------------
+
 	@Override protected void processEntity( Entity entity, float deltaTime ) {
+		// batcher.draw( texture, x, y );
 	}
 
 	//--------------------------------------------------------------------------
