3c9f1691d3b01c6c4e8f46c5486496246e518332
Alexander Prime <rainesabaal@gmail.com>
2016-09-18 18:02:04 -0700

    Add PlayerControllerComponent

---
+++ core/src/stream/alwaysbecrafting/septembersun/component/PlayerControllerComponent.java
@@ -0,0 +1,23 @@
+package stream.alwaysbecrafting.septembersun.component;
+
+import com.badlogic.ashley.core.Component;
+import com.badlogic.gdx.Input;
+
+//==============================================================================
+public class PlayerControllerComponent implements Component {
+	//--------------------------------------------------------------------------
+
+	int btn_a = Input.Keys.Z;
+	int btn_b = Input.Keys.X;
+
+	int btn_up    = Input.Keys.UP;
+	int btn_down  = Input.Keys.DOWN;
+	int btn_left  = Input.Keys.LEFT;
+	int btn_right = Input.Keys.RIGHT;
+
+	int btn_start  = Input.Keys.ESCAPE;
+	int btn_select = Input.Keys.TAB;
+
+	//--------------------------------------------------------------------------
+}
+//------------------------------------------------------------------------------
