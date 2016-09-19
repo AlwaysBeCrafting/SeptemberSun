29d489391dd156909b0457f650086e73bd4ed47b
Alexander Prime <rainesabaal@gmail.com>
2016-09-18 17:23:23 -0700

    Take a Texture in SpriteComponent constructor

--- core/src/stream/alwaysbecrafting/septembersun/SeptemberSun.java
+++ core/src/stream/alwaysbecrafting/septembersun/SeptemberSun.java
@@ -1,8 +1,12 @@
 package stream.alwaysbecrafting.septembersun;
 
 import com.badlogic.ashley.core.Engine;
+import com.badlogic.ashley.core.Entity;
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
 
+import stream.alwaysbecrafting.septembersun.component.PositionComponent;
+import stream.alwaysbecrafting.septembersun.component.SpriteComponent;
 import stream.alwaysbecrafting.septembersun.system.RenderSystem;
 import stream.alwaysbecrafting.septembersun.system.SpriteRenderSystem;
 
@@ -22,6 +26,16 @@ public class SeptemberSun extends ApplicationAdapter {
 
 		engine.addSystem( new RenderSystem() );
 		engine.addSystem( SpriteRenderSystem.create() );
+
+
+		Texture tex = new Texture( "fella.png" );
+
+		Entity e = new Entity();
+
+		e.add( new PositionComponent() );
+		e.add( new SpriteComponent( tex ));
+
+		engine.addEntity( e );
 	}
 
 	//--------------------------------------------------------------------------
