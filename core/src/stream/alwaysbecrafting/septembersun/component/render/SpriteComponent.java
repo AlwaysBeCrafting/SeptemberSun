0f3235d152393da65233b874932d5f581d2a2426
Alexander Prime <rainesabaal@gmail.com>
2016-10-09 15:42:34 -0700

    Add origin coords to sprite/bbox

--- core/src/stream/alwaysbecrafting/septembersun/component/render/SpriteComponent.java
+++ core/src/stream/alwaysbecrafting/septembersun/component/render/SpriteComponent.java
@@ -1,16 +1,27 @@
 package stream.alwaysbecrafting.septembersun.component.render;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 
 //==============================================================================
 public class SpriteComponent {
 	//--------------------------------------------------------------------------
 
 	public Texture sprite;
+	public Vector2 origin;
 
 	//--------------------------------------------------------------------------
 
-	public SpriteComponent( Texture tex ) { sprite = tex; }
+	public SpriteComponent( Texture tex ) {
+		this( tex, 0, 0 );
+	}
+
+	//--------------------------------------------------------------------------
+
+	public SpriteComponent( Texture tex, int originX, int originY ) {
+		sprite = tex;
+		origin = new Vector2( originX, originY );
+	}
 
 	//--------------------------------------------------------------------------
 }
