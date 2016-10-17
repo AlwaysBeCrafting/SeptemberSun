3700319d442db0adec6c30e63fe136b9098efb3a
Alexander Prime <rainesabaal@gmail.com>
2016-10-16 17:19:26 -0700

    Don't allow colliding objects to pass through each other

--- core/src/stream/alwaysbecrafting/septembersun/system/physics/SolidCollisionHandlerSystem.java
+++ core/src/stream/alwaysbecrafting/septembersun/system/physics/SolidCollisionHandlerSystem.java
@@ -4,7 +4,12 @@ import stream.alwaysbecrafting.flare.Entity;
 import stream.alwaysbecrafting.flare.EntitySystem;
 import stream.alwaysbecrafting.septembersun.component.physics.CollisionComponent;
 import stream.alwaysbecrafting.septembersun.component.physics.PositionComponent;
-import stream.alwaysbecrafting.septembersun.util.Log;
+import stream.alwaysbecrafting.septembersun.component.physics.VelocityComponent;
+
+import static stream.alwaysbecrafting.septembersun.data.Collision.BOTTOM;
+import static stream.alwaysbecrafting.septembersun.data.Collision.LEFT;
+import static stream.alwaysbecrafting.septembersun.data.Collision.RIGHT;
+import static stream.alwaysbecrafting.septembersun.data.Collision.TOP;
 
 //==============================================================================
 public class SolidCollisionHandlerSystem extends EntitySystem {
@@ -13,6 +18,7 @@ public class SolidCollisionHandlerSystem extends EntitySystem {
 	@Override protected boolean acceptEntity( Entity entity ) {
 		return entity.hasAll(
 				PositionComponent.class,
+				VelocityComponent.class,
 				CollisionComponent.class );
 	}
 
@@ -20,10 +26,26 @@ public class SolidCollisionHandlerSystem extends EntitySystem {
 
 	@Override protected void onHandleEntity( Entity entity, double deltaTime ) {
 		PositionComponent positionComp = entity.get( PositionComponent.class );
+		VelocityComponent velocityComp = entity.get( VelocityComponent.class );
 		CollisionComponent collisionComp = entity.get( CollisionComponent.class );
 
 		collisionComp.collisions.forEach( collision -> {
-			Log.d( "A collision has occured!" );
+			if ( collision.quadrant == LEFT ) {
+				positionComp.x += collision.intersection.width;
+				velocityComp.h = 0;
+			}
+			if ( collision.quadrant == TOP ) {
+				positionComp.y -= collision.intersection.height;
+				velocityComp.v = 0;
+			}
+			if ( collision.quadrant == RIGHT ) {
+				positionComp.x -= collision.intersection.width;
+				velocityComp.h = 0;
+			}
+			if ( collision.quadrant == BOTTOM ) {
+				positionComp.y += collision.intersection.height;
+				velocityComp.v = 0;
+			}
 		} );
 	}
 
